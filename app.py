dde6b3f5e301845df5a38582dabacade5e5b35d3
Mitra <aniruddha.mitra@stryker.com>
2022-08-02 15:54:48 +0530

    int: updates to the database

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 # /// = relative path, //// = absolute path
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
-
+from models import Todo
 
 @app.route("/")
 def home():
